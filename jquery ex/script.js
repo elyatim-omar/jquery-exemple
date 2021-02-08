8e9ceb0810dd8908df195dc891f3853cb4d22bfc
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-02-08 10:32:54 +0100

    exemple 1

---
+++ jquery ex/script.js
@@ -0,0 +1,9 @@
+$("#text").ready(function() {
+    $("#text").hover(function() {
+        $(this).css("color","black");
+    });
+    $("#text").click( function() {
+        alert("congratulations");
+    });
+
+});
