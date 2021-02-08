ae7a0df0a50d8cf3d3773fa9112323aeadcc53f7
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-02-08 12:38:20 +0100

    version3

--- jquery ex/script.js
+++ jquery ex/script.js
@@ -1,6 +1,6 @@
 $("document").ready(function() {
     $("#text").hover(function() {
-        $(this).css("color","black");
+        $("h1").css("color","black");
     });
     $("#text").click( function() {
         alert("tu as trouver la deuxieme fonction");
