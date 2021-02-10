b9b07a1cb3576fbd4577a298860898fde59ee310
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-02-10 10:08:17 +0100

    v 3

--- jquery ex/script.js
+++ jquery ex/script.js
@@ -1,9 +1,37 @@
 $("document").ready(function() {
     $("#text").hover(function() {
-        $("h1").css("color","black");
+        $(this).css("color","black");
     });
     $("#text").click( function() {
         alert("tu as trouver la deuxieme fonction");
     });
 
 });
+
+// objet
+
+var emp = {
+    name: "Zara",
+    age: 10
+ };
+ console.log(emp);
+
+// scope 
+var myVar = "global";     // ==> Declare a global variable
+
+//clossures
+function create() {
+    var counter = 0;
+     
+    return {
+       increment: function() {
+          counter++;
+       },
+        print: function() {
+          console.log(counter);
+       }
+    }
+ }
+ var c = create();
+c.increment();
+c.print();  
